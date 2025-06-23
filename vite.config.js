3fc9ceca0ce0a28286367ac43f5d2103e1a3da93
QuocPhung <quocphung2005@gmail.com>
2025-06-23 09:04:58 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',       // Quan trọng: cho phép truy cập từ ngoài
+        port: 5173,            // Có thể đổi nếu bị chiếm
+        strictPort: true,      // Báo lỗi nếu port bị chiếm
+    },
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
     ],
 });
